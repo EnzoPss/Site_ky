7c944c8a721f5e4f88911fd901225583017022cf
EnzoPss <enzopousse.06@gmail.com>
2026-03-26 18:08:14 +0100

    patch

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,12 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { Header } from "./components/header/header";
 import { Footer } from "./components/footer/footer";
 import { Profil } from "./components/profil/profil";
-import { Competences } from "./components/competences/competences";
+//import { Competences } from "./components/competences/competences";
 import { Parcours } from "./components/parcours/parcours";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Footer, Profil, Competences, Parcours],
+  imports: [RouterOutlet, Header, Footer, Profil, /*Competences,*/ Parcours],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
